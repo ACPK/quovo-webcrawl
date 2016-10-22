5b8e936ca5152f751033374580739dd8c50b6d41
Pokegear <christoph.ding@gmail.com>
2016-10-22 16:50:12 -0400

    Extracts fields elegantly.

--- webscrape.py
+++ webscrape.py
@@ -4,38 +4,32 @@ from bs4 import BeautifulSoup as Soup
 url = 'https://www.sec.gov/Archives/edgar/data/1166559/000110465916139781/0001104659-16-139781.txt'
 
 def getSoupFromURL(url):
-  print('getting url...')  
   html = requests.get(url).content
   document = Soup(html, 'html.parser')
   return document
 
 def generateCSV(soup):
-  print('generatingCSV...\n')
-  gatherColumnNames()
+  # the 'rows' of the table are made up of <infotable> tags which do not describe any other elements in the xml
+  # each row will have the same fields
+  rows = soup.find_all('infotable')
+  fields = gatherColumnNames(rows[0])
 
-def gatherColumnNames():
-  print('gathering column names')
+def hasNoNestedColumns(column):
+  return len(column.findChildren()) == 0
 
+def extractNestedColumns(child):
+  if hasNoNestedColumns(child):
+    if child is None:
+      return 0
+    else:
+      return child
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+def gatherColumnNames(row):
+  children = row.findChildren()
+  # we want a list without the divs that wrap nested columns  
+  columns = map(extractNestedColumns, children)
+  cleanColumns = [x for x in columns if x != None]
+  return cleanColumns
 
 def main():
   filing = getSoupFromURL(url)
