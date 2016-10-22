c9799974ddba95af853d941afaa25ccd20003d5e
Pokegear <christoph.ding@gmail.com>
2016-10-22 14:27:21 -0400

    Stubbing out functions neccesary to get

--- webscrape.py
+++ webscrape.py
@@ -1,11 +1,44 @@
 import requests
-
-print('webscrapping...')
+from bs4 import BeautifulSoup as Soup
 
 url = 'https://www.sec.gov/Archives/edgar/data/1166559/000110465916139781/0001104659-16-139781.txt'
 
-response = requests.get(url)
-html = response.content
+def getSoupFromURL(url):
+  print('getting url...')  
+  html = requests.get(url).content
+  document = Soup(html, 'html.parser')
+  return document
 
-print(html)
+def generateCSV(soup):
+  print('generatingCSV...\n')
+  gatherColumnNames()
 
+def gatherColumnNames():
+  print('gathering column names')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def main():
+  filing = getSoupFromURL(url)
+  csv = generateCSV(filing)
+
+main()
